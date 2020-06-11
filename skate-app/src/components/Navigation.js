4fb0022b357af2c82daa1a3397f4882bab08c8d7
Pace Davis <pacemdavis@gmail.com>
2020-06-10 23:33:03 -0500

    progress

--- skate-app/src/components/Navigation.js
+++ skate-app/src/components/Navigation.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 import blackMobileLogo from '../images/blackMobileLogo.png'
 import whiteMobileLogo from '../images/whiteMobileLogo.png'
 import Menu from './Menu'
+import mobileMenuImage from '../images/mobileMenuImage.jpg'
 
 
 const NavButton = ({onClick}) => 
@@ -21,7 +22,31 @@ const NavButton = ({onClick}) =>
 // </button> 
 
 const Dropdown = ({show}) =>
-<div style={{visibility: show ? "visible" : "hidden", backgroundColor: "blue", position: "absolute", height: "100%", width: "100%"}}>
+<div  style={{
+  visibility: show ? "visible" : "hidden", 
+  backgroundImage: `url(${mobileMenuImage})`,
+  backgroundSize: 'cover', 
+  position: "absolute",
+  backgroundPosition: '75% 50%', 
+  height: "100%", 
+  width: "100%",
+  transition: 'margin 1s',
+  margin: '0 0 0 -10vh',
+  }}>
+  <div className="overlay">
+    <ul className='navItemsMobile' >  
+      <img src={whiteMobileLogo} />
+      <li className ="mobileNavList">
+        Map
+      </li>
+      <li className ="mobileNavList">
+        Add Spot
+      </li>
+      <li className ="mobileNavList">
+        Logout
+      </li>
+    </ul>
+  </div>
 </div>
 
 
